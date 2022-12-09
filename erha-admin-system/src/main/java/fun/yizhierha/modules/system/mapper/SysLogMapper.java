1cf52d74ff054c23344455695eb7e45067a68043
Forever2ha <1539280617@qq.com>
2022-12-09 20:58:02 +0800

    fix: 移除一些提前生成的代码

--- erha-admin-system/src/main/java/fun/yizhierha/modules/system/mapper/SysLogMapper.java
+++
@@ -1,9 +0,0 @@
-package fun.yizhierha.modules.system.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import fun.yizhierha.modules.system.domain.SysLog;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysLogMapper extends BaseMapper<SysLog> {
-}
--- erha-admin-system/src/main/java/fun/yizhierha/modules/system/service/SysLogService.java
+++
@@ -1,8 +0,0 @@
-package fun.yizhierha.modules.system.service;
-
-import fun.yizhierha.modules.system.domain.SysLog;
-import com.baomidou.mybatisplus.extension.service.IService;
-public interface SysLogService extends IService<SysLog>{
-
-
-}

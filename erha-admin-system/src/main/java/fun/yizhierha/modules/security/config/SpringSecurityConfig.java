7dfd1d695493ca580dc438624c90f76368ae487c
Forever2ha <1539280617@qq.com>
2022-12-17 19:52:55 +0800

    feat: 新增[系统监控：SQL监控]

--- erha-admin-system/src/main/java/fun/yizhierha/modules/security/config/SpringSecurityConfig.java
+++ erha-admin-system/src/main/java/fun/yizhierha/modules/security/config/SpringSecurityConfig.java
@@ -125,7 +125,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/avatar/**").permitAll()
                 .antMatchers("/api/file/**").permitAll()
                 // 阿里巴巴 druid
-                .antMatchers("/api/druid/**").permitAll()
+                .antMatchers("/druid/**").permitAll()
                 // 放行OPTIONS请求
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 // 自定义匿名访问所有url放行：允许匿名和带Token访问，细腻化到每个 Request 类型
